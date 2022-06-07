0d7821050f91d00c699011df213ddad07e9c800d
sas2055 <107055765+sas2055@users.noreply.github.com>
2022-06-07 22:08:47 +0530

    number_system.py

---
+++ number_system.py
@@ -0,0 +1,20 @@
+"""
+# number system
+n = 120
+print(type(n))
+print('decimal to binary:',bin(n))
+print('decimal to octal:',oct(n))
+print('decimal to hexadecimal:',hex(n))
+
+print('binary to decimal:',0b1111000)
+print('binary to octal:',oct(0b1111000))
+print('binary to hexadecimal:',hex(0b1111000))
+
+print('octal to decimal:',0o170)
+print('octal to hexadecimal:',hex(0o170))
+print('octal to binary:',bin(0o170))
+
+print('hexadecimal to decimal:', 0x78)
+print('hexadecimal to binary:',bin(0x78))
+print('hexadecimal to octal:',oct(0x78))
+"""
