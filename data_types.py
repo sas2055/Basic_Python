0bbdbb21d70f70bc2d08f1878d86c89daf535da9
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-06-26 21:20:03 +0530

    data_types.py

---
+++ data_types.py
@@ -0,0 +1,34 @@
+"""
+# Que.1 conversion of string
+s = '190'
+print(type(s))
+print(dict.fromkeys(s,'number'))
+print(int(s))
+print(float(s))
+print(complex(s))
+print(bool(s))
+print(list(s))
+print(tuple(s))
+print(set(s))
+print(bytes(s))
+print(bytearray(s))
+print(bin(s))
+print(oct(s))
+print(hex(s))
+print(type(s))
+print(dict.fromkeys(s))
+===============================================
+
+
+# Que.2 conversion of string and integer
+s = '190'
+k = int(s)    # k is an integer
+print(bytes(k))
+print(bytearray(k))
+print(bin(k))
+print(oct(k))
+print(hex(k))
+print(frozenset(s))
+print(type(s))
+=============================================
+"""
