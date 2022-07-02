572dae01b0cf14b7c604ab82688da1ffaaac4c09
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-07-02 22:58:10 +0530

    unicode_with_smiley_faces.py

---
+++ unicode_with_smiley_faces.py
@@ -0,0 +1,68 @@
+# Unicode:
+# Every emoji has a Unicode associated with it.
+# Emojis also have a CLDR short name, which can also be used.
+# From the list of unicodes, replace '+' with '000'
+# prefix the unicode with '\' and print it.
+# syntax: '\U+codes'
+#####################################################################################
+
+# Examples
+print('grinning face is: ', '\U0001F600')                              # grinning face is:  😀
+print('beaming face with smiling eyes is: ', '\U0001F601')             # beaming face with smiling eyes is:  😁
+print('face with tears of joy is: ', '\U0001F602')                     # face with tears of joy is:  😂
+print('grinning face with big eyes is: ', '\U0001F603')                # grinning face with big eyes is:  😃
+print('grinning face with smiling eyes is: ', '\U0001F604')            # grinning face with smiling eyes is:  😄
+print('grinning face with sweat is: ', '\U0001F605')                   # grinning face with sweat is:  😅
+print('grinning squinting face is: ', '\U0001F606')                    # grinning squinting face is:  😆
+print('smiling face with halo is: ', '\U0001F607')                     # smiling face with halo is:  😇
+print('winking face	is: ', '\U0001F609')                               # winking face	is:  😉
+print('neutral face is: ', '\U0001F610')                               # neutral face is:  😐
+
+print('expressionless face is: ', '\U0001F611')                        # expressionless face is:  😑
+print('unamused face is: ', '\U0001F612')                              # unamused face is:  😒
+print('pensive face is: ', '\U0001F614')                               # pensive face is:  😔
+print('kissing face is: ', '\U0001F617')                               # kissing face is:  😗
+print('face blowing a kiss is: ', '\U0001F618')                        # face blowing a kiss is:  😘
+print('kissing face with smiling eyes is: ', '\U0001F619')             # kissing face with smiling eyes is:  😙
+
+print('sleeping face is: ', '\U0001F634')                              # sleeping face is:  😴
+print('face without mouth is: ', '\U0001F636')                         # face without mouth is:  😶
+print('face with medical mask is: ', '\U0001F637')                     # face with medical mask is:  😷
+print('slightly smiling face is: ', '\U0001F642')                      # slightly smiling face is:  🙂
+print('upside-down face is: ', '\U0001F643')                           # upside-down face is:  🙃
+print('face with rolling eyes is: ', '\U0001F644')                     # face with rolling eyes is:  🙄
+
+print('smiling face with smiling eyes is: ', '\U0001F60A')             # smiling face with smiling eyes is:  😊
+print('face savoring food is: ', '\U0001F60B')                         # face savoring food is:  😋
+print('relieved face is: ', '\U0001F60C')                              # relieved face is:  😌
+print('smiling face with heart-eye is: ', '\U0001F60D')                # smiling face with heart-eye is:  😍
+print('smirking face is: ', '\U0001F60F')                              # smirking face is:  😏
+
+print('kissing face with closed eyes is: ', '\U0001F61A')              # kissing face with closed eyes is:  😚
+print('face with tongue is: ', '\U0001F61B')                           # face with tongue is:  😛
+print('winking face with tongue is: ', '\U0001F61C')                   # winking face with tongue is:  😜
+print('squinting face with tongue is: ', '\U0001F61D')                 # squinting face with tongue is:  😝
+
+print('sleepy face is: ', '\U0001F62A')                                # sleepy face is:  😪
+print('grimacing face is: ', '\U0001F62C')                             # grimacing face is:  😬
+print('smiling face is: ', '\U0001F63A')                               # smiling face is:  😺
+
+print('zany face is: ', '\U0001F92A')                                  # zany face is:  🤪
+print('shushing face is: ', '\U0001F92B')                              # shushing face is:  🤫
+print('face with hand over mouth is: ', '\U0001F92D')                  # face with hand over mouth is:  🤭
+
+print('zipper-mouth face is: ', '\U0001F910')                          # zipper-mouth face is:  🤐
+print('money-mouth face is: ', '\U0001F911')                           # money-mouth face is:  🤑
+print('face with thermometer is: ', '\U0001F912')                      # face with thermometer is:  🤒
+print('thinking face is: ', '\U0001F914')                              # thinking face is:  🤔
+print('face with head-bandage is: ', '\U0001F915')                     # face with head-bandage is:  🤕
+print('hugging face is: ', '\U0001F917')                               # hugging face is:  🤗
+
+print('nauseated face is: ', '\U0001F922')                             # nauseated face is:  🤢
+print('rolling on the floor laughing is: ', '\U0001F923')              # rolling on the floor laughing is:  🤣
+print('drooling face is: ', '\U0001F924')                              # drooling face is:  🤤
+print('lying face is: ', '\U0001F925')                                 # lying face is:  🤥
+print('face with raised eyebrow is: ', '\U0001F928')                   # face with raised eyebrow is:  🤨
+print('star-struck is: ', '\U0001F929')                                # star-struck is:  🤩
+print('smiling face with 3 hearts is: ', '\U0001F970')                 # smiling face with 3 hearts is:  🥰
+###############################################################################
