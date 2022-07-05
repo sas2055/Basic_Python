32a9a6ee871168eb60af3d3a9376b113b6a73db4
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-07-05 18:20:09 +0530

    none.py

---
+++ none.py
@@ -0,0 +1,38 @@
+"""
+# None:
+    - its a keyword
+    - Nothing, null, empty stub, no value
+    - it is not the same as 0, False, or an empty string.
+    - None is a data type of its own (NoneType) and only None can be None.
+------------------------------------------------------------
+
+
+# we will check the type of None
+print(type(None)
+-----------------------------------------------------------
+
+k = [1, None, None, 10]
+print(k)
+print(k[1])
+-----------------------------------------------------------
+
+# declaring a variable as None
+var = None
+
+# checking it is value
+if var is None:
+    print("var has a value of None")
+else:
+    print("var has a value")
+------------------------------------------------------------
+
+x = None
+
+if x:
+  print('Do you think None is True?')
+elif x is False:
+  print ('Do you think None is False?')
+else:
+  print('None is not True, or False, None is just None...')
+===========================================================
+"""
