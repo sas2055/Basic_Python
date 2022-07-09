275073cc2fd122134d263cb0345ea011ffbe7cae
Shital_Ajit_Shelar <107055765+sas2055@users.noreply.github.com>
2022-07-09 09:54:51 +0530

    python.py

---
+++ python.py
@@ -0,0 +1,90 @@
+"""
+# python:
+    - it is a simple, high level, general purpose and object-oriented
+    programming langauge.
+    - it is a programming langauge with objects, modules, threads, exceptions
+    and automatic memory management.
+    - it used to code read-ability
+----------------------------------------------------------------------
+
+# python designed by:
+    - python invented by Guido van Rossum
+  Started:- 1989
+  first appeared:
+    - 20 february 1991
+------------------------------------------------------------------------
+# python developer:
+    - python software foundation
+-------------------------------------------------------------------
+# website:
+    - www.python.org
+-----------------------------------------------------------------------
+
+# Features in Python:
+    1. It is very easy to code in python language.
+    2. Free and Open Source.
+    3. Object-Oriented Language.
+    4. GUI(Graphical User interfaces) Programming Support.
+    5. High-Level Language.
+    6. Python is  Extensible language.
+    7. Python is Portable language.
+    8. Python is Integrated language.
+    9. Python is an Interpreted Language because Python code is executed line by line at a time.
+    10. Python has a large standard library that provides a rich set of modules and functions
+    11. Dynamically Typed Language.
+    12. Frontend and backend development
+    13. It is also a developer-friendly language.
+-----------------------------------------------------------------------
+
+# Advantages:
+    - Presence of third-party modules
+    - Extensive support libraries(NumPy for numerical calculations, Pandas for data analytics, etc.)
+    - Open source and large active community base
+    - Easy to read, learn and write
+    - User-friendly data structures
+    - High-level language
+    - Data typed language
+    - Object-Oriented and Procedural Programming language
+    - Portable and Interactive
+    - Ideal for prototypes – provide more functionality with less coding
+    - Highly Efficient
+    - Internet of Things(IoT) Opportunities
+    - Interpreted Language
+    - Portable across Operating systems
+---------------------------------------------------------------------------
+
+# What’s the Difference Between Python 2.x and Python 3.x?
+
+Basis of comparison	              Python 3	                           Python 2
+
+Release Date	                   2008	                                2000
+
+Function	                  print (“hello”)	                       print “hello”
+
+Division of Integers	  Whenever two integers are divided,        When two integers are divided,
+                            you get a float value.                  you always provide integer value
+
+Unicode	                default storing of strings is Unicode.	    To store Unicode string value,
+                                                                    you require to define them with "u".
+
+Syntax	                The syntax is simpler and easily            The syntax is difficult to under-stand.
+                            under-stand.
+
+Rules of ordering       Rules of ordering comparisons have         Rules of ordering comparison are very complex.
+Comparisons		            been simplified.
+
+Iteration		        The new Range() function introduced         the xrange() is used for iterations.
+                            to perform iterations.
+
+Exceptions	            It should be enclosed in a parenthesis.	        It should be enclosed in notations.
+
+Leak of variables	    The value of variables never changes.	        The value of the global variable will
+                                                                    change while using it inside for-loop.
+
+Backward 	                Not difficult to port python 2              Python version 3 is not backwardly
+compatibility	          to python 3, but it is never reliable.           compatible with Python 2.
+
+Library	              Many recent developers are creating libraries   Many older libraries created for Python 2
+                        which you can only use with Python 3.	        is not forward-compatible.
+=========================================================================================================
+"""
